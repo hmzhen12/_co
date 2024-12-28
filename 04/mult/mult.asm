d958896de4d856a611e4b3adf8fd233c669fb33d
Fel <162543367+hmzhen12@users.noreply.github.com>
2024-12-28 22:44:38 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -8,30 +8,29 @@
 
 // Put your code here.
   @2
-	M=0 //set RAM[2] value into 0
+	M=0 
 	@0
 	D=M
 	@31
-	D;JEQ //check if RAM[0] == 0, jump into the conclusion RAM[2] = 0. Because 0 * x = 0
+	D;JEQ 
 	@1
 	D=M
 	@31
-	D;JEQ //check if RAM[1] == 0, jump into the conclusion RAM[2] = 0. Because x * 0 = 0
+	D;JEQ 
 	@18
-	D;JGT // else if RAM[1] is greater than 0, jump to line 14
-	@0 //else if RAM[1] is negative
-	M=!M //set RAM[0] value into negative
-	M=M+1 //Add RAM[0] value by 1, because M = !M would be M = -1 * (M+1).. e.x (M = 5; !M = -6)
+	D;JGT 
+	@0 
+	M=!M 
+	M=M+1 
 	@1
-	M=!M //set RAM[1] = !RAM[1] so the value would be positive
-	M=M+1 //Just like before, we add !M value by 1
-	@0 //start the looping program to multiplies R0 and R1
+	M=!M 
+	M=M+1 
+	@0 
 	D=M
 	@2
-	M=D+M //set RAM[2] = RAM[2] + RAM[0]
+	M=D+M 
 	@1
-	M=M-1 //decrease the total loop that need to do
+	M=M-1 
 	D=M 
 	@18
-	D;JGT //if the loop value haven't reach 0, the program would go back to line 25
-	//the loop has finished and the final result is in RAM[2]
+	D;JGT 
